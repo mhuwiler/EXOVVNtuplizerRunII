9bdf7fbd89462ceee55857dd4c6817afc5af1e90
Marc Huwiler <marc.huwiler@cern.ch>
2021-03-12 11:25:52 +0100

    Added helper script for uploading tuples

---
+++ Ntuplizer/uploadsamples.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mounteos
+cp flatTuple100k.root /eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/
+cp flatTuple10000.root /eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/
+cp flatTuple10000.root /eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/
+
